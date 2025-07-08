280338aedc6f50e8fdc7d6dc0627008496f29b5a
Ken501 <kenneth_young501@live.com>
2025-07-08 09:18:15 -0400

    Update func

--- file/file.go
+++ file/file.go
@@ -7,10 +7,13 @@ import (
 )
 
 func ViewFileContents(n string) {
+	s := make([]byte, 128)
 	f, err := os.Open(n)
 	if err != nil {
 		fmt.Printf("Error: %v occurred at: %v", err, time.Now())
 	}
 	fmt.Printf("Value: %v Type: %T\n", f, f)
+	data, err := f.Read(s)
+	fmt.Println(data)
 
 }
